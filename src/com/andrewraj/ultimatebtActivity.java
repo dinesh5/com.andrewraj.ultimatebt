af55c86a61dc383fffe7ee7b161ab029583c34d9
Andrew Raj <me@andrewraj.com>
2012-04-11 16:06:40 +0800

    First commit

---
+++ src/com/andrewraj/ultimatebtActivity.java
@@ -0,0 +1,14 @@
+package com.andrewraj;
+
+import com.andrewraj.ultimatebt.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ultimatebtActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
